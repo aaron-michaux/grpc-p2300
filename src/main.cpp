35b254406fbefbd6e285dcaee5c455ef6caf27f1
Aaron Michaux <aaron@pageofswords.net>
2023-01-01 11:23:31 -0500

    close to working

--- src/main.cpp
+++ src/main.cpp
@@ -41,13 +41,13 @@ int main(int, char**) {
   std::thread server_thread{GreetingServer::run_server};
 
   sgrpc::ExecutionContext ctx{2, 1};
-  ctx.run();
-
   stdexec::scheduler auto sched = sgrpc::Scheduler{ctx};
 
   auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
 
-  Greeting::Client client{ctx, channel};
+  Greeting::Client client{ctx, channel}; // TODO: create with a scheduler (?)
+  ctx.run();
+
   helloworld::HelloRequest request;
   request.set_name("Pius!");
 
@@ -55,14 +55,17 @@ int main(int, char**) {
   auto fun = [](int i) { return compute(i); };
   auto work = stdexec::when_all(stdexec::on(sched, stdexec::just(0) | stdexec::then(fun)),
                                 stdexec::on(sched, stdexec::just(1) | stdexec::then(fun)),
-                                stdexec::on(sched, stdexec::just(2) | stdexec::then(fun))) //
-              | client.say_hello(std::move(request));
+                                stdexec::on(sched, stdexec::just(2) | stdexec::then(fun)));
+
+  auto snd = client.say_hello(std::move(request));
 
   auto result = client.sync_say_hello("Metellus");
   fmt::print("{}\n", result);
 
   // Launch the work and wait for the result:
   auto [i, j, k] = stdexec::sync_wait(std::move(work)).value();
+
+  stdexec::sync_wait(std::move(snd));
 
   // Print the results:
   fmt::print("{}, {}, {}\n", i, j, k);
